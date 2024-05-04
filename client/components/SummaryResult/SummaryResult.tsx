d8282613afc84feae65035eac98fbfd846cfd569
hectarek <hector@stiegleredtech.org>
2024-05-04 19:08:37 -0400

    added error messaging and adjusted to be syncronous server actions

--- client/components/SummaryResult/SummaryResult.tsx
+++ client/components/SummaryResult/SummaryResult.tsx
@@ -7,16 +7,20 @@ import { useEffect, useState } from "react";
 function SummaryResult({ data }: SummaryResultsProps) {
 	const [apiResult, setApiResult] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState("");
 
 	useEffect(() => {
 
         if (!data) return;
 
+		console.log("This should be the url", data)
+
 		const getAPI = async () => {
             setIsLoading(true); 
+            setError(""); 
 
 			try {
-				const res = await fetch("https://youtube-summarizer-server-hectarek-hectareks-projects.vercel.app/api/summarize", {
+				const res = await fetch("http://localhost:8080/api/summarize", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
@@ -34,6 +38,7 @@ function SummaryResult({ data }: SummaryResultsProps) {
 				setApiResult(responseData.summary);
 			} catch (error) {
 				console.error("Error:", error);
+				setError('There was an error. Please try again!')
 			} finally {
                 setIsLoading(false);
             }
@@ -44,7 +49,13 @@ function SummaryResult({ data }: SummaryResultsProps) {
 	return (
 		<section className="flex flex-col">
 			<h2 className="text-2xl font-bold pb-6">Generated Summary:</h2>
-            {isLoading ? <Loader className="w-8 h-8 animate-spin"/> : <div className="max-w-[1280px]" dangerouslySetInnerHTML={{ __html: apiResult }} />}
+			{isLoading ? (
+                <Loader className="w-8 h-8 animate-spin" />
+            ) : error ? (
+                <div className="text-red-500">{error}</div> 
+            ) : (
+                <div className="max-w-[1280px]" dangerouslySetInnerHTML={{ __html: apiResult }} />
+            )}		
 		</section>
 	);
 }
