1d3e9f526646aa3ce76fb06eaed220abf7f85904
hectarek <hector@stiegleredtech.org>
2024-05-04 19:11:06 -0400

    really need to add prod vs dev url calls

--- client/components/SummaryResult/SummaryResult.tsx
+++ client/components/SummaryResult/SummaryResult.tsx
@@ -6,42 +6,41 @@ import { useEffect, useState } from "react";
 
 function SummaryResult({ data }: SummaryResultsProps) {
 	const [apiResult, setApiResult] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState("");
 
 	useEffect(() => {
+		if (!data) return;
 
-        if (!data) return;
-
-		console.log("This should be the url", data)
+		console.log("This should be the url", data);
 
 		const getAPI = async () => {
-            setIsLoading(true); 
-            setError(""); 
+			setIsLoading(true);
+			setError("");
 
 			try {
-				const res = await fetch("http://localhost:8080/api/summarize", {
+				const res = await fetch("https://youtube-summarizer-server-hectarek-hectareks-projects.vercel.app/api/summarize", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
 					},
-                    credentials: 'include',
-					body: JSON.stringify({ url: data.values}),
+					credentials: "include",
+					body: JSON.stringify({ url: data.values }),
 				});
 
-                if (!res.ok) {
-                    throw new Error('Network response was not ok');
-                }
+				if (!res.ok) {
+					throw new Error("Network response was not ok");
+				}
 
 				const responseData = await res.json();
 				console.log("Success:", responseData);
 				setApiResult(responseData.summary);
 			} catch (error) {
 				console.error("Error:", error);
-				setError('There was an error. Please try again!')
+				setError("There was an error. Please try again!");
 			} finally {
-                setIsLoading(false);
-            }
+				setIsLoading(false);
+			}
 		};
 		getAPI();
 	}, [data]);
@@ -49,13 +48,7 @@ function SummaryResult({ data }: SummaryResultsProps) {
 	return (
 		<section className="flex flex-col">
 			<h2 className="text-2xl font-bold pb-6">Generated Summary:</h2>
-			{isLoading ? (
-                <Loader className="w-8 h-8 animate-spin" />
-            ) : error ? (
-                <div className="text-red-500">{error}</div> 
-            ) : (
-                <div className="max-w-[1280px]" dangerouslySetInnerHTML={{ __html: apiResult }} />
-            )}		
+			{isLoading ? <Loader className="w-8 h-8 animate-spin" /> : error ? <div className="text-red-500">{error}</div> : <div className="max-w-[1280px]" dangerouslySetInnerHTML={{ __html: apiResult }} />}
 		</section>
 	);
 }
